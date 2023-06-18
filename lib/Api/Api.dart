cd33670abe60410bc0c6754766086a674c5c2110
Fadi-h <62059908+Fadi-h@users.noreply.github.com>
2023-06-18 10:49:37 +0300

    fadi

--- lib/Api/Api.dart
+++ lib/Api/Api.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 import '../model/number.dart';
 
 class Api {
-  static var url = "http://10.0.2.2:8000";
+  static var url = "http://10.0.2.2:8080";
   // static var url = "http://192.168.1.10:8080";
 
   static Future<List<Number>> getNumbers() async {
